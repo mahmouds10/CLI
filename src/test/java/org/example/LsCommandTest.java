d48efb292e6997192bc70ac28bcd25e6aa5c1a08
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2024-10-25 15:20:22 +0300

    Fix error in ls -a

--- src/test/java/org/example/LsCommandTest.java
+++ src/test/java/org/example/LsCommandTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -22,6 +23,7 @@ class LsCommandTest {
     @BeforeEach
     void setUp() {
         lsCommand = new LsCommand();
+        CLI.currentPath = System.getProperty("user.dir");
 
         // Set up the output stream to capture System.out
         outputStream = new ByteArrayOutputStream();
@@ -35,8 +37,34 @@ class LsCommandTest {
         System.setOut(originalOut);
     }
 
+    // Test the hidden files option
+    @Test
+    @Order(1)
+    void TestHiddenFiles() {
+        // Execute the 'ls' command
+        lsCommand.execute("ls -a");
+
+        // Get the output from the output stream
+        String actualOutput = outputStream.toString();
+
+        // Define the expected output
+        String expectedOutput = ".git\n"
+                + ".gitignore\n"
+                + ".idea\n"
+                + "Class_Diagram.png\n"
+                + "pom.xml\n"
+                + "src\n"
+                + "target\n"
+                + "Testing";
+        assertEquals(
+                // Convert newline from windows "\r\n" to unix "\n"
+                expectedOutput.replace("\r\n", "\n").trim(),
+                actualOutput.replace("\r\n", "\n").trim());
+    }
+
     // Test the wrong usage case
     @Test
+    @Order(3)
     void TestWrongUsage() {
         // Execute the 'ls' command
         lsCommand.execute("ls -s");
@@ -53,6 +81,7 @@ class LsCommandTest {
 
     // Test the normal usage case
     @Test
+    @Order(2)
     void TestNormalUsage() {
         // Execute the 'ls' command
         lsCommand.execute("ls");
@@ -67,41 +96,18 @@ class LsCommandTest {
                 + "pom.xml\n"
                 + "src\n"
                 + "target\n"
-                + "Text_Files";
+                + "Testing";
 
         assertEquals(
                 // Convert newline from windows "\r\n" to unix "\n"
                 expectedOutput.replace("\r\n", "\n").trim(),
                 actualOutput.replace("\r\n", "\n").trim());
 
-    }
-
-    // Test the hidden files option
-    @Test
-    void TestHiddenFiles() {
-        // Execute the 'ls' command
-        lsCommand.execute("ls");
-
-        // Get the output from the output stream
-        String actualOutput = outputStream.toString();
-
-        // Define the expected output
-        String expectedOutput = ".git\n"
-                + ".gitignore\n"
-                + ".idea\n"
-                + "Class_Diagram.png\n"
-                + "pom.xml\n"
-                + "src\n"
-                + "target\n"
-                + "Text_Files";
-        assertEquals(
-                // Convert newline from windows "\r\n" to unix "\n"
-                expectedOutput.replace("\r\n", "\n").trim(),
-                actualOutput.replace("\r\n", "\n").trim());
     }
 
     // Test the recursive files option
     @Test
+    @Order(4)
     void TestRecursiveLsCommand() {
         // Change to the testing directory
         CdCommand cdCommand = new CdCommand();
