91bb7c2e0c73238e8d3518dcc83c2cb80cd945a0
Bsabeso <102309089+Bsabeso@users.noreply.github.com>
2024-10-30 23:04:38 +0300

    Add files via upload

---
+++ src/test/java/org/example/TestMkdir.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestMkdir {
+    Command c;
+
+    // Save the original System.out to reuse after the test
+    private final PrintStream originalOut = System.out;
+
+    // create a stream to capture the output instead of printing in default stream
+    private ByteArrayOutputStream outputStream;
+    @BeforeEach
+    void setUp() {
+        Main.currentPath = System.getProperty("user.dir");
+
+        // Set up the output stream to capture System.out
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    // Post test teardown
+    @AfterEach
+    void tearDown() {
+        // Restore System.out to its original state
+        System.setOut(originalOut);
+    }
+    @Test
+    public void testMkdir(){
+        c = new MkdirCommand();
+        c.execute("mkdir files");
+
+        c = new LsCommand();
+        c.execute("ls");
+        String actualOutput = outputStream.toString();
+        String[] directories = actualOutput.split("\r\n");
+
+        assertTrue(Arrays.asList(directories).contains("files"));
+    }
+}
